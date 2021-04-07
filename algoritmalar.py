e05f2e4c07e3cb1d29b05d14e1ac58ac5155cb41
batuhank34 <33568252+batuhank34@users.noreply.github.com>
2021-04-08 00:04:56 +0300

    dugum giris - cikis derecesi

--- algoritmalar.py
+++ algoritmalar.py
@@ -1,9 +1,9 @@
 class Graph:
-    def __init__(self, Nodes):
-        self.nodes = Nodes
+    def __init__(self):
+        self.nodes = []
         self.adjList = {}
-        for node in self.nodes:
-            self.adjList[node] = []
+        # for node in self.nodes:
+        #     self.adjList[node] = []
 
     def printAdj(self):
         for node in self.nodes:
@@ -14,32 +14,79 @@ class Graph:
         # self.adjList[kenar].append(kose)
 
     def convertToAdjacenyList(self):
-
         f = open("Komsuluk Matrisi.txt", "r")
 
         matrisSatiri = 0
         matrisSutun = 0
+        karakter = 65
+        karakter1 = 65
 
         for dosyaSatiri in f.read().splitlines():
             # matrix.append([int(x) for x in satir.split(' ')])
             matrisSutun = 0
+            karakter1 = 65
+
+            # self.adjList[chr(karakter)] = []  # satir
+            # self.nodes.append(chr(karakter))  # satir
+
+            self.adjList[matrisSatiri] = []  # satir
+            self.nodes.append(matrisSatiri)  # satir
             for x in dosyaSatiri.split(' '):
+
                 if(x == "1"):
 
-                    self.addEdge(matrisSatiri, matrisSutun)
-                    #graph.addEdge(matrisSatiri, matrisSutun)
+                    # self.addEdge(chr(karakter), chr(karakter1))  # satir  sutun
+                    self.addEdge(matrisSatiri, matrisSutun)  # satir  sutun
+
+                    # graph.addEdge(matrisSatiri, matrisSutun)
                 matrisSutun += 1
+                karakter1 += 1
             matrisSatiri += 1
+            karakter += 1
         f.close()
+
+    def girisCikisSayilari(self, aranan):
+
+        cikisSayac = 0
+        girisSayac = 0
+
+        for key, item in self.adjList.items():
+
+            # cevirimli kenar mi
+            if aranan in item and aranan == key:
+                cikisSayac += 2
+                girisSayac += 1  # cevirimli kenar asagida lende bri tanesini icine aliyor buradad 1 +2 oldu
+            elif aranan in item:
+                cikisSayac += 1
+        girisSayac += len(self.adjList[aranan])
+        print("Cikis Sayac: ", cikisSayac, " Giris Sayac: ", girisSayac)
 
 
 def main():
-    # matrisin 5x5 oldugu icin
-    nodes = [0, 1, 2, 3, 4]
-    graph = Graph(nodes)
+    graph = Graph()
 
     graph.convertToAdjacenyList()
     graph.printAdj()
+    print("--------")
+
+    graph.girisCikisSayilari(0)  # aranan degeri parametre olarak veriyoruz.
+
+    # aranan = 3
+
+    # cikisSayac = 0
+    # girisSayac = 0
+
+    # for key, item in graph.adjList.items():
+
+    #     # cevirimli kenar mi
+    #     if aranan in item and aranan == key:
+    #         cikisSayac += 2
+    #         girisSayac += 2
+    #     elif aranan in item:
+    #         cikisSayac += 1
+    #     elif aranan == key:
+    #         girisSayac += len(item)
+    # print(cikisSayac)
 
     # f = open("Komsuluk Matrisi.txt", "r")
     # matrix = []
