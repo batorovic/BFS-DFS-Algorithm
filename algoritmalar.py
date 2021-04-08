0ce0a1fdcd6e63c52ba11267714ed9077e39a28e
batuhank34 <33568252+batuhank34@users.noreply.github.com>
2021-04-08 23:29:41 +0300

    graf turu ve kenar sayisi

--- algoritmalar.py
+++ algoritmalar.py
@@ -2,6 +2,8 @@ class Graph:
     def __init__(self):
         self.nodes = []
         self.adjList = {}
+        self.kenarSayisi = 0
+        self.komsulukMatrix = []
         # for node in self.nodes:
         #     self.adjList[node] = []
 
@@ -11,6 +13,7 @@ class Graph:
 
     def addEdge(self, kose, kenar):
         self.adjList[kose].append(kenar)
+
         # self.adjList[kenar].append(kose)
 
     def convertToAdjacenyList(self):
@@ -20,6 +23,8 @@ class Graph:
         matrisSutun = 0
         karakter = 65
         karakter1 = 65
+
+        matrix = []
 
         for dosyaSatiri in f.read().splitlines():
             # matrix.append([int(x) for x in satir.split(' ')])
@@ -33,7 +38,7 @@ class Graph:
             self.nodes.append(matrisSatiri)  # satir
             for x in dosyaSatiri.split(' '):
 
-                if(x == "1"):
+                if x == "1":
 
                     # self.addEdge(chr(karakter), chr(karakter1))  # satir  sutun
                     self.addEdge(matrisSatiri, matrisSutun)  # satir  sutun
@@ -47,19 +52,64 @@ class Graph:
 
     def girisCikisSayilari(self, aranan):
 
-        cikisSayac = 0
         girisSayac = 0
+        cikisSayac = 0
+
+        cikisSayac += len(self.adjList[aranan])
 
         for key, item in self.adjList.items():
 
             # cevirimli kenar mi
             if aranan in item and aranan == key:
-                cikisSayac += 2
-                girisSayac += 1  # cevirimli kenar asagida lende bri tanesini icine aliyor buradad 1 +2 oldu
-            elif aranan in item:
-                cikisSayac += 1
-        girisSayac += len(self.adjList[aranan])
-        print("Cikis Sayac: ", cikisSayac, " Giris Sayac: ", girisSayac)
+                girisSayac += 2
+                cikisSayac += 1  # cevirimli kenar asagida lende bri tanesini icine aliyor buradad 1 +2 oldu
+            if aranan in item:
+                girisSayac += 1
+
+        print("Cikis Derecesi: ", cikisSayac, " Giris Derecesi: ", girisSayac)
+
+    def kenarSayilari(self):
+        matrix = []
+        kenarSayisi = 0
+        sutun = 0
+
+        with open("Komsuluk Matrisi.txt") as inputFile:
+            for line in inputFile:
+                matrix.append([int(x) for x in line.split(' ')])
+
+        x = 0
+        y = 0
+
+        grafTuru = "YONLU"
+        for x, row in enumerate(matrix):
+            for y, column in enumerate(matrix):
+                # son satirdan bır onceki satirda zaten son satiri kontrol ettim o yuzden satir sutun birbirine esit olmamalı.
+                if matrix[x][y] == 1 and matrix[y][x] == 1 and y != x:
+                    grafTuru = "YONSUZ"
+            if grafTuru == "YONSUZ":
+                break
+        print(grafTuru)
+        if grafTuru == "YONSUZ":
+            for x, line in enumerate(matrix):
+                # TXT olarak verilen komsulum matrisi yonsuz graf oldugu icin simetrik matris oluyor. Bunun icin kosegen uzerinde 1 varsa onu da kenar sayisina ekledim.
+                if matrix[x][x] == 1:
+                    kenarSayisi += 1
+                # alt kosegenden kenar sayisini buldum.
+                for y in range(sutun):
+                    if matrix[x][y] == 1:
+                        kenarSayisi += 1
+                sutun += 1
+            print("Kenar Sayisi : ", kenarSayisi)
+
+        elif grafTuru == "YONLU":
+
+            for x, row in enumerate(matrix):
+                for y, column in enumerate(matrix):
+                    if matrix[x][y] == 1:
+                        kenarSayisi += 1
+            print("kenar : ", kenarSayisi)
+
+        inputFile.close()
 
 
 def main():
@@ -70,6 +120,12 @@ def main():
     print("--------")
 
     graph.girisCikisSayilari(0)  # aranan degeri parametre olarak veriyoruz.
+
+    print("--------")
+
+    graph.kenarSayilari()
+
+    print("--------")
 
     # aranan = 3
 
@@ -101,12 +157,9 @@ def main():
     #             graph.addEdge(matrisSatiri, matrisSutun)
     #         matrisSutun += 1
     #     matrisSatiri += 1
-
     # for x in range(5):
     #     for y in range(5):
     #         if(matrix[x][y] == 1):
     #             graph.addEdge(x, y)
-
-
 if __name__ == "__main__":
     main()
